6159456d38ae35f7ecaf596d1476dcc86f444ebd
Your Name <you@example.com>
2026-06-16 15:21:47 +0300

    fix: add missing ssr, referrer, url params to Metrika init

--- app/layout.tsx
+++ app/layout.tsx
@@ -221,7 +221,7 @@ export default async function RootLayout({
               for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
             })(window,document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
-            ym(${YM_COUNTER_ID},'init',{clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true,ecommerce:'dataLayer'});
+            ym(${YM_COUNTER_ID},'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:'dataLayer',referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});
           `}
         </Script>
         <noscript>
